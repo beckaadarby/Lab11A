459e91cf4f8eb3ef09d81774c3a0624bd3474080
Terzakis <10951@vtsd.com>
2020-01-21 08:33:09 -0500

    80 point version

--- Lab11avst.java
+++ Lab11avst.java
@@ -15,6 +15,12 @@ public class Lab11avst
 		final int MAX = 100;
 		boolean primes[];
 		primes = new boolean[MAX];
+
+		for(int index = 2; index < primes.length; index++){
+			primes[index] = true;
+		}
+
+
 		computePrimes(primes);
 		displayPrimes(primes);
 	}
@@ -22,17 +28,27 @@ public class Lab11avst
 	public static void computePrimes(boolean primes[])
 	{
 		System.out.println("\nCOMPUTING PRIME NUMBERS");
-
+		for(int index = 2; index < primes.length; index++){
+			for(int k= (index +1); k< primes.length; k++){
+				if (primes[k]==true){
+					int remainder = k%index;
+					if (remainder == 0){
+						primes[k] = false;
+					}
+				}
+			}
+		}
 	}
 
 	public static void displayPrimes(boolean primes[])
 	{
 		System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
 		System.out.println();
-
+		for(int index = 2; index < primes.length; index ++){
+			if (primes[index] == true){
+					System.out.print(index + " ");
+			}
+		}
 	}
 
 }
-
-
-
